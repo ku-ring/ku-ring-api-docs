02a9c8cac9eb8f987ecdd93fab5024a63cc9ad14
HanJu Kim <kuring.korea@gmail.com>
2026-01-19 17:42:45 +0900

    26.01.19 기준 Stoplight 최신화

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjMwMzYxNg/spectral.js?branch_slug=main&token=f6867b93-3174-4adb-9f8a-935d413cc40c";
+export default { extends: ruleset };
